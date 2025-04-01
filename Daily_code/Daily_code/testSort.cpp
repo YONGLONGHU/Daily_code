098c7b0964f3e9c85519c9a68b264a1b11f33b68
HUYONGLONG <2830915693@qq.com>
2025-04-01 10:29:00 +0800

    C++17any

--- Daily_code/Daily_code/testSort.cpp
+++ Daily_code/Daily_code/testSort.cpp
@@ -72,3 +72,37 @@
 //
 //    return 0;
 //}
+#include <iostream>
+#include <string>
+#include <any>
+int main()
+{
+	{
+		std::any a = 10;
+		std::any b = 88.88;
+		std::any c = std::string("bitejiuyeke");
+		int* aa = std::any_cast<int>(&a);
+		std::cout << *aa << std::endl;
+		std::cout << *std::any_cast<double>(&b) << std::endl;
+		std::cout << *std::any_cast<std::string>(&c) << std::endl;
+	}
+	{
+		Test d("Leihou");
+		std::any any_d = d;
+		std::any any_e(d);
+		std::any any_f(any_d);
+		std::any any_g = any_d;
+	}
+	{
+		std::cout << "---------------any之间相互的赋-----\n";
+		std::any any_f;
+		any_f = 33;
+		std::cout << *std::any_cast<int>(&any_f) << std::endl;
+		std::string c = "Hello World";
+		any_f = c;
+		std::cout << *std::any_cast<std::string>(&any_f) << std::endl;
+		any_f = std::any(Test("test"));
+		std::cout << std::any_cast<Test>(&any_f)->_data << std::endl;
+	}
+	return 0;
+}
