5ac5fb68b19cf7c3a669cb460c6cbaa69f50d4c2
HUYONGLONG <2830915693@qq.com>
2025-04-16 15:45:23 +0800

    TcpServer

--- Daily_code/Daily_code/符号字符.c
+++ Daily_code/Daily_code/符号字符.c
@@ -165,121 +165,193 @@
 //    uint64_t u64;
 //} epoll_data_t;
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/epoll.h>
-#include <fcntl.h>
+//#include <stdio.h>
+//#include <stdlib.h>
+//#include <string.h>
+//#include <unistd.h>
+//#include <sys/socket.h>
+//#include <netinet/in.h>
+//#include <sys/epoll.h>
+//#include <fcntl.h>
+//
+//#define MAX_EVENTS 10
+//#define PORT 8080
+//
+//void setnonblocking(int sockfd) {
+//    int flags = fcntl(sockfd, F_GETFL, 0);
+//    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
+//}
+//
+//int main() {
+//    int listen_sock, conn_sock, nfds, epollfd;
+//    struct sockaddr_in server_addr, client_addr;
+//    socklen_t client_len = sizeof(client_addr);
+//    struct epoll_event ev, events[MAX_EVENTS];
+//    char buffer[1024];
+//
+//    // 创建监听套接字
+//    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+//    if (listen_sock == -1) {
+//        perror("socket");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // 设置地址重用
+//    int opt = 1;
+//    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+//
+//    // 绑定地址
+//    memset(&server_addr, 0, sizeof(server_addr));
+//    server_addr.sin_family = AF_INET;
+//    server_addr.sin_addr.s_addr = INADDR_ANY;
+//    server_addr.sin_port = htons(PORT);
+//
+//    if (bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+//        perror("bind");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // 监听
+//    if (listen(listen_sock, SOMAXCONN) == -1) {
+//        perror("listen");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // 创建 epoll 实例
+//    epollfd = epoll_create1(0);
+//    if (epollfd == -1) {
+//        perror("epoll_create1");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // 添加监听套接字到 epoll
+//    ev.events = EPOLLIN;
+//    ev.data.fd = listen_sock;
+//    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
+//        perror("epoll_ctl: listen_sock");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    // 事件循环
+//    for (;;) {
+//        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
+//        if (nfds == -1) {
+//            perror("epoll_wait");
+//            exit(EXIT_FAILURE);
+//        }
+//
+//        for (int n = 0; n < nfds; ++n) {
+//            if (events[n].data.fd == listen_sock) {
+//                // 新连接
+//                conn_sock = accept(listen_sock,
+//                    (struct sockaddr*)&client_addr,
+//                    &client_len);
+//                if (conn_sock == -1) {
+//                    perror("accept");
+//                    continue;
+//                }
+//
+//                setnonblocking(conn_sock);
+//                ev.events = EPOLLIN | EPOLLET;  // 边缘触发模式
+//                ev.data.fd = conn_sock;
+//                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
+//                    perror("epoll_ctl: conn_sock");
+//                    close(conn_sock);
+//                }
+//            }
+//            else {
+//                // 已连接套接字有数据可读
+//                int fd = events[n].data.fd;
+//                ssize_t count;
+//
+//                while ((count = read(fd, buffer, sizeof(buffer))) > 0) {
+//                    // 处理数据
+//                    write(STDOUT_FILENO, buffer, count);
+//
+//                    // 简单回显
+//                    write(fd, buffer, count);
+//                }
+//
+//                if (count == 0 || (count == -1 && errno != EAGAIN)) {
+//                    // 连接关闭或出错
+//                    close(fd);
+//                }
+//            }
+//        }
+//    }
+//
+//    close(listen_sock);
+//    return 0;
+//}
 
-#define MAX_EVENTS 10
-#define PORT 8080
+class TcpServer {
+private:
+    uint64_t _next_id;      //这是一个自动增长的连接ID
+    int _port;
+    int _timeout;           //这是非活跃连接的统计时间---多长时间无通信就是非活跃连接
+    bool _enable_inactive_release;//是否启动了非活跃连接超时销毁的判断标志
+    EventLoop _baseloop;    //这是主线程的EventLoop对象，负责监听事件的处理
+    Acceptor _acceptor;    //这是监听套接字的管理对象
+    LoopThreadPool _pool;   //这是从属EventLoop线程池
+    std::unordered_map<uint64_t, PtrConnection> _conns;//保存管理所有连接对应的shared_ptr对象
 
-void setnonblocking(int sockfd) {
-    int flags = fcntl(sockfd, F_GETFL, 0);
-    fcntl(sockfd, F_SETFL, flags | O_NONBLOCK);
-}
-
-int main() {
-    int listen_sock, conn_sock, nfds, epollfd;
-    struct sockaddr_in server_addr, client_addr;
-    socklen_t client_len = sizeof(client_addr);
-    struct epoll_event ev, events[MAX_EVENTS];
-    char buffer[1024];
-
-    // 创建监听套接字
-    listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_sock == -1) {
-        perror("socket");
-        exit(EXIT_FAILURE);
+    using ConnectedCallback = std::function<void(const PtrConnection&)>;
+    using MessageCallback = std::function<void(const PtrConnection&, Buffer*)>;
+    using ClosedCallback = std::function<void(const PtrConnection&)>;
+    using AnyEventCallback = std::function<void(const PtrConnection&)>;
+    using Functor = std::function<void()>;
+    ConnectedCallback _connected_callback;
+    MessageCallback _message_callback;
+    ClosedCallback _closed_callback;
+    AnyEventCallback _event_callback;
+private:
+    void RunAfterInLoop(const Functor& task, int delay) {
+        _next_id++;
+        _baseloop.TimerAdd(_next_id, delay, task);
     }
-
-    // 设置地址重用
-    int opt = 1;
-    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    // 绑定地址
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(PORT);
-
-    if (bind(listen_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        perror("bind");
-        exit(EXIT_FAILURE);
+    //为新连接构造一个Connection进行管理
+    void NewConnection(int fd) {
+        _next_id++;
+        PtrConnection conn(new Connection(_pool.NextLoop(), _next_id, fd));
+        conn->SetMessageCallback(_message_callback);
+        conn->SetClosedCallback(_closed_callback);
+        conn->SetConnectedCallback(_connected_callback);
+        conn->SetAnyEventCallback(_event_callback);
+        conn->SetSrvClosedCallback(std::bind(&TcpServer::RemoveConnection, this, std::placeholders::_1));
+        if (_enable_inactive_release) conn->EnableInactiveRelease(_timeout);//启动非活跃超时销毁
+        conn->Established();//就绪初始化
+        _conns.insert(std::make_pair(_next_id, conn));
     }
-
-    // 监听
-    if (listen(listen_sock, SOMAXCONN) == -1) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    // 创建 epoll 实例
-    epollfd = epoll_create1(0);
-    if (epollfd == -1) {
-        perror("epoll_create1");
-        exit(EXIT_FAILURE);
-    }
-
-    // 添加监听套接字到 epoll
-    ev.events = EPOLLIN;
-    ev.data.fd = listen_sock;
-    if (epoll_ctl(epollfd, EPOLL_CTL_ADD, listen_sock, &ev) == -1) {
-        perror("epoll_ctl: listen_sock");
-        exit(EXIT_FAILURE);
-    }
-
-    // 事件循环
-    for (;;) {
-        nfds = epoll_wait(epollfd, events, MAX_EVENTS, -1);
-        if (nfds == -1) {
-            perror("epoll_wait");
-            exit(EXIT_FAILURE);
+    void RemoveConnectionInLoop(const PtrConnection& conn) {
+        int id = conn->Id();
+        auto it = _conns.find(id);
+        if (it != _conns.end()) {
+            _conns.erase(it);
         }
-
-        for (int n = 0; n < nfds; ++n) {
-            if (events[n].data.fd == listen_sock) {
-                // 新连接
-                conn_sock = accept(listen_sock,
-                    (struct sockaddr*)&client_addr,
-                    &client_len);
-                if (conn_sock == -1) {
-                    perror("accept");
-                    continue;
-                }
-
-                setnonblocking(conn_sock);
-                ev.events = EPOLLIN | EPOLLET;  // 边缘触发模式
-                ev.data.fd = conn_sock;
-                if (epoll_ctl(epollfd, EPOLL_CTL_ADD, conn_sock, &ev) == -1) {
-                    perror("epoll_ctl: conn_sock");
-                    close(conn_sock);
-                }
-            }
-            else {
-                // 已连接套接字有数据可读
-                int fd = events[n].data.fd;
-                ssize_t count;
-
-                while ((count = read(fd, buffer, sizeof(buffer))) > 0) {
-                    // 处理数据
-                    write(STDOUT_FILENO, buffer, count);
-
-                    // 简单回显
-                    write(fd, buffer, count);
-                }
-
-                if (count == 0 || (count == -1 && errno != EAGAIN)) {
-                    // 连接关闭或出错
-                    close(fd);
-                }
-            }
-        }
     }
-
-    close(listen_sock);
-    return 0;
-}
+    //从管理Connection的_conns中移除连接信息
+    void RemoveConnection(const PtrConnection& conn) {
+        _baseloop.RunInLoop(std::bind(&TcpServer::RemoveConnectionInLoop, this, conn));
+    }
+public:
+    TcpServer(int port) :
+        _port(port),
+        _next_id(0),
+        _enable_inactive_release(false),
+        _acceptor(&_baseloop, port),
+        _pool(&_baseloop) {
+        _acceptor.SetAcceptCallback(std::bind(&TcpServer::NewConnection, this, std::placeholders::_1));
+        _acceptor.Listen();//将监听套接字挂到baseloop上
+    }
+    void SetThreadCount(int count) { return _pool.SetThreadCount(count); }
+    void SetConnectedCallback(const ConnectedCallback& cb) { _connected_callback = cb; }
+    void SetMessageCallback(const MessageCallback& cb) { _message_callback = cb; }
+    void SetClosedCallback(const ClosedCallback& cb) { _closed_callback = cb; }
+    void SetAnyEventCallback(const AnyEventCallback& cb) { _event_callback = cb; }
+    void EnableInactiveRelease(int timeout) { _timeout = timeout; _enable_inactive_release = true; }
+    //用于添加一个定时任务
+    void RunAfter(const Functor& task, int delay) {
+        _baseloop.RunInLoop(std::bind(&TcpServer::RunAfterInLoop, this, task, delay));
+    }
+    void Start() { _pool.Create();  _baseloop.Start(); }
+};
