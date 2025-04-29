434d5301383d0f0b05a1bad5673a01fdfca04060
HUYONGLONG <2830915693@qq.com>
2025-04-29 20:13:54 +0800

    字符串 有效的括号 每日一题

--- Daily_code/Daily_code/testSort.cpp
+++ Daily_code/Daily_code/testSort.cpp
@@ -1375,3 +1375,25 @@ public:
 		return result;
 	}
 };
+
+class Solution {
+public:
+	bool isValid(string s) {
+		stack<char> stk;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '(')
+				stk.push(')');
+			else if (s[i] == '[')
+				stk.push(']');
+			else if (s[i] == '{')
+				stk.push('}');
+			else if (!stk.empty() && s[i] == stk.top()) {
+				stk.pop();
+			}
+			else {
+				return false;
+			}
+		}
+		return stk.empty();
+	}
+};
