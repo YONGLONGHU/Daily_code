59dca9af2749211a04d02252454276f2be425313
HUYONGLONG <2830915693@qq.com>
2025-05-16 10:35:43 +0800

    1

--- Daily_code/Daily_code/test.cpp
+++ Daily_code/Daily_code/test.cpp
@@ -141,118 +141,179 @@ string LongString(string s)
 //		cout << e << " ";
 //	return 0;
 //}
-#include<unordered_set>
-#include<algorithm>
-string LengthOfSubstring(string s)
-{
-	unordered_set<char> occ;
-	int right = 0;
-	int maxlen = 0;
-	int size = s.size();
-	int start = 0;
-	for (int i = 0; i < size; i++)
-	{
-		if (i != 0)
-			occ.erase(s[i-1]);
-		while (right < size && !occ.count(s[right]))
-			occ.insert(s[right++]);
-		if (maxlen < right - i)
-		{
-			maxlen = right - i;
-			start = i;
-		}
-	}
-	return s.substr(start, maxlen);
-}
+//#include<unordered_set>
+//#include<algorithm>
+//string LengthOfSubstring(string s)
+//{
+//	unordered_set<char> occ;
+//	int right = 0;
+//	int maxlen = 0;
+//	int size = s.size();
+//	int start = 0;
+//	for (int i = 0; i < size; i++)
+//	{
+//		if (i != 0)
+//			occ.erase(s[i-1]);
+//		while (right < size && !occ.count(s[right]))
+//			occ.insert(s[right++]);
+//		if (maxlen < right - i)
+//		{
+//			maxlen = right - i;
+//			start = i;
+//		}
+//	}
+//	return s.substr(start, maxlen);
+//}
+//
+//string LengthOfSubstring1(string s)
+//{
+//	int right = 0;
+//	unordered_set<char> st;
+//	int maxlen = 1;
+//	int start = 0;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		if (i != 0)
+//			st.erase(s[i - 1]);
+//		while (right < s.size() && !st.count(s[right]))
+//		{
+//			st.insert(s[right]);
+//			right++;
+//		}
+//		if (right - i > maxlen)
+//		{
+//			maxlen = right - i;
+//			start = i;
+//		}
+//	}
+//	return s.substr(start, maxlen);
+//}
+//#include<stack>
+//string ReaverseString(string ss)
+//{
+//	int n = ss.size();
+//	stack<string> st;
+//	string result;
+//	for (int i = 0; i < n;)
+//	{
+//		while (ss[i] == ' ')
+//			i++;
+//		int next = i;
+//		while (ss[next] != ' ')
+//			next++;
+//		string temp = ss.substr(i, next - i);
+//		st.push(temp);
+//		i = next;
+//	}
+//	while (!st.empty())
+//	{
+//		result += st.top();
+//		st.pop();
+//		if (!st.empty())
+//			result += ' ';
+//	}
+//	return result;
+//}
+//
+//class Singleton
+//{
+//public:
+//	static Singleton& GetInstance()
+//	{
+//		return instance;
+//	}
+//	Singleton(const Singleton&) = delete;
+//	Singleton& operator=(const Singleton&) = delete;
+//private:
+//	Singleton();
+//	volatile static Singleton instance;
+//};
+//Singleton Singleton::instance;
+//
+//class Singleton1
+//{
+//public:
+//	static Singleton1* GetInstance()
+//	{
+//		if (instance == nullptr)
+//		{
+//			mtx.lock();
+//			if (instance == nullptr)
+//			{
+//				instance = new Singleton1();
+//			}
+//			mtx.unlock();
+//		}
+//		return instance;
+//	}
+//	Singleton1(const Singleton1&) = delete;
+//	Singleton1& operator=(const Singleton1&) = delete;
+//private:
+//	Singleton1();
+//	volatile static Singleton1* instance;
+//	static pthread_mutex_t mtx;
+//};
+//Singleton1* Singleton1::instance = nullptr;
+//pthread_mutex_t Singleton1::mtx;
+//
 
-string LengthOfSubstring1(string s)
+//int searchInsert(vector<int>& nums, int target)
+//{
+//	int left = 0, right = nums.size() - 1;
+//	while (left <= right)
+//	{
+//		int mid = (right + left) / 2;
+//		if (target > nums[mid])
+//			left = mid + 1;
+//		else if (target == nums[mid])
+//			return mid;
+//		else
+//			right = mid - 1;
+//	}
+//	return left;
+//}
+
+bool searchMatrix(vector<vector<int>>& matrix, int target)
 {
-	int right = 0;
-	unordered_set<char> st;
-	int maxlen = 1;
-	int start = 0;
-	for (int i = 0; i < s.size(); i++)
+	int top = 0, bottom = matrix.size();
+	while (top <= bottom)
 	{
-		if (i != 0)
-			st.erase(s[i - 1]);
-		while (right < s.size() && !st.count(s[right]))
-		{
-			st.insert(s[right]);
-			right++;
-		}
-		if (right - i > maxlen)
-		{
-			maxlen = right - i;
-			start = i;
-		}
+		int mid = (top + bottom) / 2;
+		if (target == matrix[mid][0])
+			return true;
+		else if (target > matrix[mid][0])
+			top = mid + 1;
+		else
+			bottom = mid - 1;
 	}
-	return s.substr(start, maxlen);
+	int left = 0, right = matrix[bottom].size() - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (target == matrix[bottom][mid])
+			return true;
+		else if (target > matrix[bottom][mid])
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return false;
 }
-#include<stack>
-string ReaverseString(string ss)
+#include<unordered_map>
+vector<int> topKFrenquent(vector<int>& nums, int k)
 {
-	int n = ss.size();
-	stack<string> st;
-	string result;
-	for (int i = 0; i < n;)
+	unordered_map<int, int> freqMap;
+	for (auto e : nums)
+		freqMap[e]++;
+	vector<pair<int, int>>freqVec(freqMap.begin(), freqMap.end());
+	sort(freqVec.begin(), freqVec.end(),
+		[](const pair<int, int>& a, const pair<int, int>& b) {
+			return a.second > b.second;
+		});
+	vector<int> result;
+	for (int i = 0; i < k; ++i)
 	{
-		while (ss[i] == ' ')
-			i++;
-		int next = i;
-		while (ss[next] != ' ')
-			next++;
-		string temp = ss.substr(i, next - i);
-		st.push(temp);
-		i = next;
-	}
-	while (!st.empty())
-	{
-		result += st.top();
-		st.pop();
-		if (!st.empty())
-			result += ' ';
+		result.push_back(freqVec[i].first);
 	}
 	return result;
 }
-
-class Singleton
-{
-public:
-	static Singleton& GetInstance()
-	{
-		return instance;
-	}
-	Singleton(const Singleton&) = delete;
-	Singleton& operator=(const Singleton&) = delete;
-private:
-	Singleton();
-	volatile static Singleton instance;
-};
-Singleton Singleton::instance;
-
-class Singleton1
-{
-public:
-	static Singleton1* GetInstance()
-	{
-		if (instance == nullptr)
-		{
-			mtx.lock();
-			if (instance == nullptr)
-			{
-				instance = new Singleton1();
-			}
-			mtx.unlock();
-		}
-		return instance;
-	}
-	Singleton1(const Singleton1&) = delete;
-	Singleton1& operator=(const Singleton1&) = delete;
-private:
-	Singleton1();
-	volatile static Singleton1* instance;
-	static pthread_mutex_t mtx;
-};
-Singleton1* Singleton1::instance = nullptr;
-pthread_mutex_t Singleton1::mtx;
-
