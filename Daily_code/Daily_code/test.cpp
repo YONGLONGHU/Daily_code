8669d32a59df5394adff73f0a0eadb8dc17e1e98
HUYONGLONG <2830915693@qq.com>
2025-05-20 17:55:30 +0800

    卓识面试

--- Daily_code/Daily_code/test.cpp
+++ Daily_code/Daily_code/test.cpp
@@ -28,43 +28,43 @@ using namespace std;
 //    return 0;
 //}
 
-void removeDuplicatesAndSortInPlace(std::vector<int>& nums) 
-{
-	if (nums.empty()) return;
-	int count = 1;
-	for (int i = 1; i < nums.size(); i++)
-	{
-		if (nums[count - 1] == nums[i])
-			nums[count++] = nums[i];
-	}
-	nums.resize(count);
-}
-void ExpendCenter(string& s,int left,int right, int& start, int& maxlen)
-{
-	while (left >= 0 && right < s.size() && s[left] == s[right])
-	{
-		left--;
-		right++;
-	}
-	int len = right - left;
-	if (len > maxlen)
-	{
-		start = left + 1;
-		maxlen = len;
-	}
-}
-string LongString(string s)
-{
-	if (s.empty())
-		return "";
-	int start = 0,maxlen = 1;
-	for (int i = 0; i < s.size(); i++)
-	{
-		ExpendCenter(s, i, i, start, maxlen);
-		ExpendCenter(s, i, i+1, start, maxlen);
-	}
-	return s.substr(start, maxlen);
-}
+//void removeDuplicatesAndSortInPlace(std::vector<int>& nums) 
+//{
+//	if (nums.empty()) return;
+//	int count = 1;
+//	for (int i = 1; i < nums.size(); i++)
+//	{
+//		if (nums[count - 1] == nums[i])
+//			nums[count++] = nums[i];
+//	}
+//	nums.resize(count);
+//}
+//void ExpendCenter(string& s,int left,int right, int& start, int& maxlen)
+//{
+//	while (left >= 0 && right < s.size() && s[left] == s[right])
+//	{
+//		left--;
+//		right++;
+//	}
+//	int len = right - left;
+//	if (len > maxlen)
+//	{
+//		start = left + 1;
+//		maxlen = len;
+//	}
+//}
+//string LongString(string s)
+//{
+//	if (s.empty())
+//		return "";
+//	int start = 0,maxlen = 1;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		ExpendCenter(s, i, i, start, maxlen);
+//		ExpendCenter(s, i, i+1, start, maxlen);
+//	}
+//	return s.substr(start, maxlen);
+//}
 //int main()
 //{
 //	string s = "babad";
@@ -273,50 +273,50 @@ string LongString(string s)
 //	return left;
 //}
 
-bool searchMatrix(vector<vector<int>>& matrix, int target)
-{
-	int top = 0, bottom = matrix.size();
-	while (top <= bottom)
-	{
-		int mid = (top + bottom) / 2;
-		if (target == matrix[mid][0])
-			return true;
-		else if (target > matrix[mid][0])
-			top = mid + 1;
-		else
-			bottom = mid - 1;
-	}
-	int left = 0, right = matrix[bottom].size() - 1;
-	while (left <= right)
-	{
-		int mid = (left + right) / 2;
-		if (target == matrix[bottom][mid])
-			return true;
-		else if (target > matrix[bottom][mid])
-			left = mid + 1;
-		else
-			right = mid - 1;
-	}
-	return false;
-}
-#include<unordered_map>
-vector<int> topKFrenquent(vector<int>& nums, int k)
-{
-	unordered_map<int, int> freqMap;
-	for (auto e : nums)
-		freqMap[e]++;
-	vector<pair<int, int>>freqVec(freqMap.begin(), freqMap.end());
-	sort(freqVec.begin(), freqVec.end(),
-		[](const pair<int, int>& a, const pair<int, int>& b) {
-			return a.second > b.second;
-		});
-	vector<int> result;
-	for (int i = 0; i < k; ++i)
-	{
-		result.push_back(freqVec[i].first);
-	}
-	return result;
-}
+//bool searchMatrix(vector<vector<int>>& matrix, int target)
+//{
+//	int top = 0, bottom = matrix.size();
+//	while (top <= bottom)
+//	{
+//		int mid = (top + bottom) / 2;
+//		if (target == matrix[mid][0])
+//			return true;
+//		else if (target > matrix[mid][0])
+//			top = mid + 1;
+//		else
+//			bottom = mid - 1;
+//	}
+//	int left = 0, right = matrix[bottom].size() - 1;
+//	while (left <= right)
+//	{
+//		int mid = (left + right) / 2;
+//		if (target == matrix[bottom][mid])
+//			return true;
+//		else if (target > matrix[bottom][mid])
+//			left = mid + 1;
+//		else
+//			right = mid - 1;
+//	}
+//	return false;
+//}
+//#include<unordered_map>
+//vector<int> topKFrenquent(vector<int>& nums, int k)
+//{
+//	unordered_map<int, int> freqMap;
+//	for (auto e : nums)
+//		freqMap[e]++;
+//	vector<pair<int, int>>freqVec(freqMap.begin(), freqMap.end());
+//	sort(freqVec.begin(), freqVec.end(),
+//		[](const pair<int, int>& a, const pair<int, int>& b) {
+//			return a.second > b.second;
+//		});
+//	vector<int> result;
+//	for (int i = 0; i < k; ++i)
+//	{
+//		result.push_back(freqVec[i].first);
+//	}
+//	return result;
+//}
 
 //class Student;
 //class Person
@@ -416,38 +416,100 @@ vector<int> topKFrenquent(vector<int>& nums, int k)
 //	cout << "pb2:" << pb2 << endl;
 //}
 
-class String
-{
-public:
-	String(const char* ptr = " ")
-	{
-		_ptr = new char[strlen(ptr)+1];
-		strcpy(_ptr, ptr);
-	}
-	String(const String& s)
-	{
-		_ptr = new char[strlen(s._ptr) + 1];
-		strcpy(_ptr, s._ptr);
-	}
-	String& operator=(const String&s)
-	{
-		if (s._ptr != _ptr)
-		{
-			delete[]_ptr;
-			_ptr = nullptr;
-			_ptr = new char[strlen(s._ptr) + 1];
-			strcpy(_ptr, s._ptr);
-		}
-		return *this;
-	}
-	~String()
-	{
-		if (_ptr)
-		{
-			delete[] _ptr;
-			_ptr = nullptr;
-		}
-	}
-private:
-	char* _ptr;
-};
+//class String
+//{
+//public:
+//	String(const char* ptr = " ")
+//	{
+//		_ptr = new char[strlen(ptr)+1];
+//		strcpy(_ptr, ptr);
+//	}
+//	String(const String& s)
+//	{
+//		_ptr = new char[strlen(s._ptr) + 1];
+//		strcpy(_ptr, s._ptr);
+//	}
+//	String& operator=(const String&s)
+//	{
+//		if (s._ptr != _ptr)
+//		{
+//			delete[]_ptr;
+//			_ptr = nullptr;
+//			_ptr = new char[strlen(s._ptr) + 1];
+//			strcpy(_ptr, s._ptr);
+//		}
+//		return *this;
+//	}
+//	~String()
+//	{
+//		if (_ptr)
+//		{
+//			delete[] _ptr;
+//			_ptr = nullptr;
+//		}
+//	}
+//private:
+//	char* _ptr;
+//};
+
+//int main()
+//{
+//	int a = 1;
+//	char* c = (char*)&a;
+//	if (*c == 1)
+//		cout << "小端" << endl;
+//	else
+//		cout << "大端" << endl;
+//	return 0;
+//}
+//int SecondNum(vector<int>& nums)
+//{
+//	int largest = nums[0];
+//	int size = nums.size();
+//	int second = INT_MIN;
+//	for (int i = 0; i < size; ++i)
+//	{
+//		if (nums[i] > largest)
+//			largest = nums[i];
+//		else if (nums[i] > second)
+//		{
+//			int temp = second;
+//			second = nums[i];
+//			if (second == largest)
+//				second = temp;
+//		}
+//		return second;
+//	}
+//}
+//int main()
+//{
+//	vector<int> nums{ 2,4,5,5,6,3,2,1 };
+//	if (nums.size() < 2)
+//		cout << "没有第二大整数" << endl;
+//
+//	int result = SecondNum(nums);
+//	cout << "第二大整数为" << result << endl;
+//	return 0;
+//}
+//int MaxTemp(vector<int>& nums)
+//{
+//	int hismax = nums[0]; 
+//	int size = nums.size();
+//	int maxtemp = 0;
+//	for (int cur = 0; cur < size; cur++)
+//	{
+//		int curmax = 0;
+//		if(hismax< nums[cur])
+//			hismax = nums[cur];
+//		curmax = hismax - nums[cur];
+//		maxtemp = curmax > maxtemp ? curmax : maxtemp;
+//	}
+//	return maxtemp;
+//}
+//int main()
+//{
+//	vector<int> nums{ 5,2,1,4,6 };
+//	int result = MaxTemp(nums);
+//	cout << result << endl;
+//	return 0;
+//}
